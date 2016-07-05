ba65b1e74a33dd8a9300a71e76a37a431e29c74b
Jonathan Kestler <jkest618@gmail.com>
2016-07-05 22:04:04 +0000

    seconddraft

--- JS-Victims.js
+++ JS-Victims.js
@@ -1,3 +1,4 @@
+/* -----------victims---------------*/
 var numVictims = prompt("How many disaster victims would you like to enter?");
 
 var victimName =[]
@@ -12,5 +13,26 @@ function victimInput() {
     }
 }
 
-
+victimInput()
 console.log(victimName)
+
+/* -----------volunteers---------------*/
+
+var numVolunteers = prompt("How many disaster volunteers would you like to enter?");
+
+var volunteerName =[]
+var volunteerPhone=[]
+var volunteerStreet=[]
+
+function volunteerInput() {
+    for (var i=0; i <= numVolunteers.length; i++) {
+        volunteerName.push(prompt("Please enter your full name."));
+        volunteerPhone.push(prompt("Please enter your phone number."));
+        volunteerStreet.push(prompt("Please enter your street."));
+    }
+}
+
+var displayVic = []
+var displayVol = []
+
+for (var)
